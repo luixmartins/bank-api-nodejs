42b421be4c717cef46a5696759bf68afade13a08
Luiz Martins <luizmartins.uemg@gmail.com>
2025-03-01 15:17:30 -0300

    Finished CRUD for owner

--- src/DAO/index.js
+++ src/DAO/index.js
@@ -20,8 +20,10 @@ const DAO = {
                 console.error('Error: ', error);
 
                 return {
-                    status: 500,
-                    message: 'Internal server error'
+                    status: 400,
+                    data: {
+                        message: 'Cannot get data from the database'
+                    }
                 }
             }
 
@@ -30,11 +32,117 @@ const DAO = {
 
             return {
                 status: 500,
-                message: 'Unable to connect to the database'
+                data: {
+                    message: 'Unable to connect to the database'
+                }
             }
         }
 
+    },
+
+    async create(query, data) {
+        try {
+            await sequelize.authenticate();
+            try {
+                const createItem = await sequelize.query(
+                    query,
+                    { replacements: data }
+                );
+
+                return {
+                    status: 201,
+                    data: createItem
+                }
+
+            } catch (error) {
+                return {
+                    status: 400,
+                    data: {
+                        message: 'Invalid data'
+                    }
+                }
+            }
+
+        } catch (error) {
+            console.error('Error: ', error);
+
+            return {
+                status: 500,
+                data: {
+                    message: 'Unable to connect to the database'
+                }
+            }
+        }
+    }, 
+
+    async update(query, data) {
+        try {
+            await sequelize.authenticate();
+            try {
+                const updateItem = await sequelize.query(
+                    query,
+                    { replacements: data }
+                );
+
+                return {
+                    status: 200,
+                    data: updateItem
+                }
+
+            } catch (error) {
+                return {
+                    status: 400,
+                    data: {
+                        message: 'Invalid data'
+                    }
+                }
+            }
+
+        } catch (error) {
+            console.error('Error: ', error);
+
+            return {
+                status: 500,
+                data: {
+                    message: 'Unable to connect to the database'
+                }
+            }
+        }
+    }, 
+
+    async delete(query, data) {
+        try {
+            await sequelize.authenticate();
+            try {
+                const deleteItem = await sequelize.query(
+                    query,
+                    { replacements: data }
+                );
+
+                return {
+                    status: 200,
+                    data: deleteItem
+                }
+
+            } catch (error) {
+                return {
+                    status: 400,
+                    data: {
+                        message: 'Invalid data'
+                    }
+                }
+            }
+
+        } catch (error) {
+            console.error('Error: ', error);
+
+            return {
+                status: 500,
+                data: {
+                    message: 'Unable to connect to the database'
+                }
+            }
+        }
     }
 }
-
 export default DAO;
--- src/routes/app.js
+++ src/routes/app.js
@@ -1,16 +1,53 @@
 import express from 'express';
-import DAO from '../DAO/index.js';
+import Owner from '../controller/owner.js';
 
 const app = express();
 const port = 3000; 
 
-app.get('/',  (req, res) => {
-    const data = DAO.list('SELECT * FROM owner');
 
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    console.log("Entrou no get")
+    const data = await Owner.listOwners();
+
+    res.status(data.status).json(data.data);
+});
+
+app.post('/',  (req, res) => {
+    const owner = req.body;
+    
+    const data =  Owner.createOwner(owner);
+    
     data.then((response) => {
-        res.status(response.status).send(response.data);
+        res.status(response.status).json(response.data);
     });
 });
+
+app.put('/owner/:cpf', (req, res) => {
+    console.log("Entrou no put")
+    const owner = {
+        cpf: req.params.cpf,
+        name: req.body.name
+    }
+
+    const data = Owner.updateOnwer(owner);
+
+    data.then((response) => {
+        res.status(response.status).json(response.data);
+    });
+});
+
+app.delete('/owner/:cpf', (req, res) => {
+    const cpf = req.params.cpf;
+
+    const data = Owner.deleteOwner(cpf);
+
+    data.then((response) => {
+        res.status(response.status).json(response.data);
+    });
+});
+
 
 app.listen(port, () => {
     console.log('Server is running on http://localhost:3000');
