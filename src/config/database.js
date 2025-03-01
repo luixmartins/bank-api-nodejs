820f6f40a2fbba61dbe24bccf8f7c17af06841e4
Luiz Martins <luizmartins.uemg@gmail.com>
2025-02-28 21:20:57 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+import { Sequelize } from '@sequelize/core';
+import { MySqlDialect } from '@sequelize/mysql';
+
+const sequelize = new Sequelize({
+  dialect: MySqlDialect,
+  database: 'bank',
+  user: 'root',
+  password: '6749',
+  host: 'localhost',
+  port: 3306,
+});
+
+export default sequelize;
